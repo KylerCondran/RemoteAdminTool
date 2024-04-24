beb1e03c205b94b19a444bf081b76bf101578fab
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-24 01:09:00 -0300

    Add project files.

---
+++ RATClient/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RATClient
+{
+    public class Program
+    {
+        static TcpClient sock = new TcpClient();
+        static IPAddress ip = IPAddress.Parse("127.0.0.1");
+        static int port = 6961;
+        static void Main(string[] args)
+        {
+            string CMD = "";          
+            Console.WriteLine("Remote Administration Tool");
+            Console.WriteLine("Type HELP For A List Of Commands");
+            Console.WriteLine("");
+            while (true)
+            {
+                Console.Write(">");
+                CMD = Console.ReadLine();
+                string[] CMDS = CMD.Split(' ');
+                switch (CMDS[0].ToLower())
+                {
+                    case "connect":
+                        Connect();
+                        break;
+                    case "disconnect":
+                        EndConnection();
+                        break;
+                    case "message":
+                        SendCommand(CMDS);
+                        break;
+                    case "help":
+                        Help();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command.");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+        static void EndConnection()
+        {
+            sock.Close();
+            Console.WriteLine("RAT Client: Not Connected");
+        }
+        static void Connect()
+        {
+            ip = IPAddress.Parse("127.0.0.1");
+            port = 6961;
+            try
+            {
+                sock = new TcpClient();
+                sock.Connect(ip, port);
+                Console.WriteLine("RAT Client: Connected To: " + ip.ToString() + ":" + port.ToString());
+            } catch (Exception e)
+            {
+                //if (My.Computer.Network.Ping(""))
+                //{
+                Console.WriteLine("Error: The server RAT is not opened.");
+                Console.WriteLine("Error: An internet connection cannot be established.");
+                //}
+            }
+        }
+        static void Help()
+        {
+            Console.WriteLine("CONNECT        Start A Connection");
+            Console.WriteLine("MESSAGE        Send A Message To The Server");
+            Console.WriteLine("DISCONNECT     End A Connection");
+            Console.WriteLine("HELP           List Commands");
+        }
+        static string SerializeToXml<T>(T obj)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(T));
+            using (var z = new StringWriter())
+            {
+                using (XmlTextWriter w = new XmlTextWriter(z) { Formatting = Formatting.Indented })
+                {
+                    x.Serialize(w, obj);
+                    return z.ToString();
+                }
+            }
+        }
+        static void SendCommand(string[] CMDS)
+        {
+            Command c = new Command();
+            c.CMD = CMDS[0];
+            int argcount = CMDS.Length - 1;
+            if (argcount != 0)
+            {
+                string[] args = new string[argcount];
+                for (int i = 0; i < argcount; i++) args[i] = CMDS[i + 1];
+                c.Args = args;
+            }
+            NetworkStream nstream = sock.GetStream();
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(SerializeToXml(c));
+            nstream.Write(sendBytes, 0, sendBytes.Length);
+            nstream.Flush();
+        }
+    }
+    [Serializable]
+    public class Command
+    {
+        public string CMD { get; set; }
+        public string[] Args { get; set; }
+    }
+}
---
+++ RATServer/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RATServer
+{
+    public class Program
+    {
+        static int port = 6961;
+        static TcpClient sock = new TcpClient();
+        static TcpListener tcpc = new TcpListener(port);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server Running...");
+            while (true)
+            {
+                while (sock.Connected == false) Listen();
+                while (sock.Connected) Check();
+            }
+        }
+        static void Listen()
+        {
+            try
+            {
+                tcpc.Start();
+                sock = new TcpClient();
+                sock = tcpc.AcceptTcpClient();
+            } catch (Exception ex) 
+            {
+
+            }
+        }
+        static T DeserializeFromXml<T>(string xml)
+        {
+            T z;
+            XmlSerializer x = new XmlSerializer(typeof(T));
+            using (TextReader y = new StringReader(xml))
+            {
+                z = (T)x.Deserialize(y);
+            }
+            return z;
+        }
+        static void Check()
+        {
+            if (sock.Connected)
+            {
+                sock.SendTimeout = 5000;
+                try
+                {
+                    NetworkStream nstream = sock.GetStream();
+                    byte[] message = new byte[sock.ReceiveBufferSize + 1];
+                    int bytesRead = 0;
+                    bytesRead = nstream.Read(message, 0, Convert.ToInt32(sock.ReceiveBufferSize));
+                    if (bytesRead == 0)
+                    {
+                        sock.Close();
+                        return;
+                    }
+                    Command c = DeserializeFromXml<Command>(Encoding.ASCII.GetString(message, 0, bytesRead));
+                    ExecuteCommand(c.CMD, c.Args);
+                }
+                catch (Exception e)
+                {
+                    Check();
+                }
+            }
+        }
+        static void ExecuteCommand(string CMD, string[] Args)
+        {
+            try
+            {
+                switch (CMD)
+                {
+                    case "message":
+                        Console.WriteLine(Args[0]);
+                        break;
+                    default:
+                        break;
+                }
+            } catch (Exception ex) 
+            { 
+
+            }
+        }
+    }
+    [Serializable]
+    public class Command
+    {
+        public string CMD { get; set; }
+        public string[] Args { get; set; }
+    }
+}
