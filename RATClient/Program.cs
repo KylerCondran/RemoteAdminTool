c8390ce98301a39af2c31e62c420d5f7ebf7a25f
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-26 06:11:42 -0300

    cleaned up unused imports

--- RATClient/Program.cs
+++ RATClient/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 
