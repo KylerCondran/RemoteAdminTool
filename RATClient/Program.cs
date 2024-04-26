bb58994bb2fb9e60260feaa3bacd877aac3b527b
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-26 10:58:28 -0300

    made connect regex waaay tighter

--- RATClient/Program.cs
+++ RATClient/Program.cs
@@ -131,7 +131,7 @@ namespace RATClient
         #region "Helper Methods"
         static bool ValidCommand(string CMD)
         {
-            Match m = Regex.Match(CMD, "^disconnect$|^info$|^processes$|^shutdown$|^clipboard$|^services$|^software$|^clear$|^help$|^exit$|^connect [0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}$|^message .+$|^run .+$|^delete .+$|^sendkeys .+$|^retrieve .+$|^search .+$|^download .+ .+$", RegexOptions.IgnoreCase);
+            Match m = Regex.Match(CMD, "^disconnect$|^info$|^processes$|^shutdown$|^clipboard$|^services$|^software$|^clear$|^help$|^exit$|^connect (?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]):([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$|^message .+$|^run .+$|^delete .+$|^sendkeys .+$|^retrieve .+$|^search .+$|^download .+ .+$", RegexOptions.IgnoreCase);
             return m.Success;
         }
         static void StartInfo()
