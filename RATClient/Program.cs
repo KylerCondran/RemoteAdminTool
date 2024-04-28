179a8a2219fe7d8d7a87da5a40b37925b8206957
kylercondran@gmail.com <kylercondran@gmail.com>
2024-04-28 14:45:33 -0300

    remove unused imports

--- RATClient/Program.cs
+++ RATClient/Program.cs
@@ -2,8 +2,6 @@
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Reflection;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
